018a39187bd48f27bdfd0c90f95368fc9711439e
Honeyoung_0117 <48305428+dudgns5845@users.noreply.github.com>
2021-08-19 13:47:57 +0900

    Scene_00_Start
    Start Scene Upload

---
+++ TreasureHunter/Assets/Rio_Folder/Scripts/SoundManager_Rio.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager_Rio : MonoBehaviour
+{
+    public static SoundManager_Rio soundManager_Rio;
+
+    public List<AudioClip> BGM_clips;
+    public List<AudioClip> EFT_clips;
+
+    public AudioSource BGM_player;
+    public AudioSource EFT_player;
+
+    public enum BGM_STATE {
+        BGM_00,
+        BGM_01,
+        BGM_02,
+        BGM_03
+    }
+
+    public enum EFT_STATE {
+        CLICK,
+    }
+
+    private void Awake()
+    {
+        if(soundManager_Rio == null)
+        {
+            soundManager_Rio = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        BGM_PLAY(BGM_STATE.BGM_00);
+    }
+
+    public void BGM_PLAY(BGM_STATE type)
+    {
+        BGM_player.clip = BGM_clips[(int)type];
+        BGM_player.Play();
+    }
+
+    public void EFT_PLAY(EFT_STATE type)
+    {
+        AudioClip clip = BGM_clips[(int)type];
+        EFT_player.PlayOneShot(clip);
+    }
+}
