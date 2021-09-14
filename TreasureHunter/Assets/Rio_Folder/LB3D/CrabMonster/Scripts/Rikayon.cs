22136fa5799100879c9ed9f96a39dc969ab7f08c
Honeyoung_0117 <48305428+dudgns5845@users.noreply.github.com>
2021-09-14 17:28:47 +0900

    Update 09 14

--- TreasureHunter/Assets/Rio_Folder/LB3D/CrabMonster/Scripts/Rikayon.cs
+++ TreasureHunter/Assets/Rio_Folder/LB3D/CrabMonster/Scripts/Rikayon.cs
@@ -24,6 +24,7 @@ public class Rikayon : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        idx = Random.Range(0, 6);
         target = GameObject.FindWithTag("Player");
         animator = GetComponentInChildren<Animator>();
         agent = GetComponent<NavMeshAgent>();
@@ -33,7 +34,7 @@ public class Rikayon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        print("현재상태 : " + m_state);
+        print(m_state);
         switch (m_state)
         {
             case STATE.PATROL:
@@ -59,11 +60,14 @@ public class Rikayon : MonoBehaviour
     //다시 걷는다
     //그러다 플레이어가 일정 간격에 오면 공격모드로 전환하고
     //따라간다
-    int idx = 0;
+    int idx;
     bool isMove = false;
     public float attackRange = 20f;
+    
     void Patrol()
     {
+        agent.stoppingDistance = 0;
+
         if (!isMove)
         {
             animator.SetTrigger("Walk_Cycle_1");
@@ -75,6 +79,7 @@ public class Rikayon : MonoBehaviour
     
         if (dir.magnitude < attackRange)
         {
+            agent.stoppingDistance = 80f;
             m_state = STATE.ATTACK;
             isMove = false;
         }
@@ -136,6 +141,67 @@ public class Rikayon : MonoBehaviour
         if(distance > attackRange)
         {
             m_state = STATE.MOVE;
+        }
+    }
+
+    // 일정시간 기다렸다가 상태를 Idle 로 전환하고 싶다.
+    // 필요속성 : damage 대기시간
+    float damageDelayTime = 1;
+    private IEnumerator Damage(Vector3 shootDirection)
+    {
+        shootDirection.y = 0;
+        //transform.position += shootDirection * 2;
+        transform.position = Vector3.Lerp(transform.position, shootDirection * 4, Time.deltaTime);
+        m_state = STATE.DAMAGE;
+        // animation 의 상태를 피격으로
+        animator.SetTrigger("Take_Damage_1");
+
+        // 대기시간 만큼 기다렸다가 
+        yield return new WaitForSeconds(damageDelayTime);
+        // 상태를 Idle 로 전환
+        m_state = STATE.PATROL;
+
+    }
+
+    // 플레이어로부터 피격 받았을때 처리할 함수
+    Vector3 knockbackPos;
+    float currentTime = 0;
+    public int hp = 10;
+    public void OnDamageProcess(Vector3 shootDirection)
+    {
+        isMove = false;
+        // 죽은 애는 또 피격처리 하고 싶지 않다.
+        if (m_state == STATE.DIE)
+        {
+            return;
+        }
+
+        // 코루틴을 종료하고 싶다.
+        StopAllCoroutines();
+
+        agent.enabled = false;
+
+        currentTime = 0;
+        hp--;
+        // 1. hp 가 0 이하면 없애고 싶다.
+        if (hp <= 0)
+        {
+            // 죽으면 충돌체 꺼버리자
+            //cc.enabled = false;
+            m_state = STATE.DIE;
+            animator.SetTrigger("Die");
+
+        }
+        // 2. 맞으면 상태를 피격으로 전환하고 싶다.
+        else
+        {
+            // 넉백 knockback
+            // 밀릴 방향
+            // P = P0 + vt
+
+            // 피격처리를 코루틴을 이용하여 처리하고 싶다.
+            StartCoroutine(Damage(shootDirection));
+
         }
     }
 
